5bf02777615a5e819b16c6758d45a051c8e74781
escorcia21 <carlosescorcia21@hotmail.com>
2022-10-01 22:34:40 -0500

    carrecion en normal_card, expanded eliminado

--- lib/widgets/cards/normal_card.dart
+++ lib/widgets/cards/normal_card.dart
@@ -7,34 +7,31 @@ class NormalCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: const LinearGradient(
-              colors: [Color(0xff009688), Color(0xffff5722)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: const Offset(-4, 2), // changes position of shadow
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: const LinearGradient(
+            colors: [Color(0xff009688), Color(0xffff5722)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          child: Center(
-            child: Image.network(
-              pokemon.image,
-              fit: BoxFit.fill,
-              width: 150,
-              height: 150,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(-4, 2), // changes position of shadow
             ),
+          ],
+        ),
+        child: Center(
+          child: Image.network(
+            pokemon.image,
+            fit: BoxFit.fill,
+            width: 150,
+            height: 150,
           ),
         ),
       ),
